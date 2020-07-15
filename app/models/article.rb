eae56b87b97e8cbe7b6377d488295a73cf257a30
Sayan Sinha <sinhasayan888@gmail.com>
2020-07-16 02:55:26 +0530

    Updated few validations and messages

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,7 @@ class Article < ApplicationRecord
   after_validation { self.body = self.body.squish }
 
   validates :author, presence: true, length: { maximum: 50, minimum: 2 }, format: { with: VALID_AUTHOR_NAME_REGEX }
-  validates :title, presence: true, length: { maximum: 60, minimum: 2 }, format: { with: VALID_TITLE_REGEX }
+  validates :title, presence: true, length: { maximum: 100, minimum: 2 }, format: { with: VALID_TITLE_REGEX }
   validates :body, presence: true, length: { minimum: 2 }
 
   scope :article_author, ->(name) { where("author LIKE ?", "%#{name}%") }

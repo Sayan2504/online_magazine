0e9332e33b172e16dabd85ab0901e03e1318521a
Sayan Sinha <sinhasayan888@gmail.com>
2020-07-16 03:58:50 +0530

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Article.new( { author: "Sayan one", title: "Title one", body: "Content one" }).save
-Article.new( { author: "Sayan two", title: "Title two", body: "Content two" }).save
-Article.new( { author: "Sayan three", title: "Title three", body: "Content three" }).save
-Article.new( { author: "Sayan four", title: "Title four", body: "Content four" }).save
+Article.new( { author: "Author one", title: "Title one", body: "Content one" }).save
+Article.new( { author: "Author two", title: "Title two", body: "Content two" }).save
+Article.new( { author: "Author three", title: "Title three", body: "Content three" }).save
+Article.new( { author: "Author four", title: "Title four", body: "Content four" }).save
